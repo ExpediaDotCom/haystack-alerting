483f54ada2342bc3e382deb7a69a983983cd25fd
kpeswani <kpeswani@expedia.com>
2018-12-07 15:12:07 +0530

    Adding subscription management

---
+++ subscription-manager/src/main/scala/com/expedia/www/haystack/subscription/manager/utils/RetryOnError.scala
@@ -0,0 +1,42 @@
+/*
+ *               Copyright 2018 Expedia, Inc.
+ *
+ *        Licensed under the Apache License, Version 2.0 (the "License");
+ *        you may not use this file except in compliance with the License.
+ *        You may obtain a copy of the License at
+ *
+ *            http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *        Unless required by applicable law or agreed to in writing, software
+ *        distributed under the License is distributed on an "AS IS" BASIS,
+ *        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *        See the License for the specific language governing permissions and
+ *        limitations under the License.
+ *
+ */
+
+package com.expedia.www.haystack.subscription.manager.utils
+
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.{FiniteDuration, _}
+
+trait RetryOnError {
+
+  private val LOGGER = LoggerFactory.getLogger(this.getClass)
+
+  def retry[T](delay: FiniteDuration = 5.seconds, retries: Int = Int.MaxValue)(op: => Future[T])(implicit ec: ExecutionContext): Future[T] = {
+    op recoverWith {
+      case e: Exception =>
+        if (retries > 0) {
+          Thread.sleep(delay.toMillis)
+          retry(delay, retries - 1)(op)
+        } else {
+          Future.failed(e)
+        }
+    }
+  }
+
+
+}
