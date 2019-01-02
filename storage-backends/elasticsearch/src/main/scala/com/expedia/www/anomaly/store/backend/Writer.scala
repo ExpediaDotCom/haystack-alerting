8960c9a5a027748ed84955bc1259ceb018efa939
keshavpeswani <keshavpeswani@gmail.com>
2019-01-02 01:22:29 -0800

    Dev kpeswani (#25)
    * Updating jmx-trans metrics
    
    * Fixing namespace of the metrics
    
    * Adding elasticsearch metrics
    
    * Fixing jar dependencies

--- storage-backends/elasticsearch/src/main/scala/com/expedia/www/anomaly/store/backend/Writer.scala
+++ storage-backends/elasticsearch/src/main/scala/com/expedia/www/anomaly/store/backend/Writer.scala
@@ -6,6 +6,7 @@ import java.util.Date
 import com.expedia.www.anomaly.store.backend.ElasticSearchStore._
 import com.expedia.www.anomaly.store.backend.api.AnomalyStore.WriteCallback
 import com.expedia.www.anomaly.store.backend.api.{Anomaly, AnomalyWithId}
+import com.expedia.www.haystack.commons.metrics.MetricsSupport
 import com.typesafe.config.Config
 import org.elasticsearch.action.ActionListener
 import org.elasticsearch.action.bulk.{BulkRequest, BulkResponse}
@@ -13,6 +14,7 @@ import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.client.RestHighLevelClient
 import org.elasticsearch.common.xcontent.XContentFactory
 import org.slf4j.Logger
+import com.codahale.metrics.Timer
 
 object Writer {
   private[backend] val MAX_RETRIES_CONFIG_KEY = "max.retries"
@@ -25,8 +27,14 @@ object Writer {
 class Writer private[backend](client: RestHighLevelClient,
                               config: Config,
                               indexNamePrefix: String,
-                              logger: Logger) {
+                              logger: Logger) extends MetricsSupport {
   import Writer._
+
+  // meter that measures the write failures
+  private val esWriteFailureMeter = metricRegistry.meter("es.write.failure")
+
+  // a timer that measures the amount of time it takes to complete one bulk write
+  private val esWriteTime = metricRegistry.timer("es.writer.time")
 
   private val maxRetries = if (config.hasPath(MAX_RETRIES_CONFIG_KEY)) config.getInt(MAX_RETRIES_CONFIG_KEY) else  DEFAULT_MAX_RETRIES
   private val retryBackOffMillis = if (config.hasPath(MAX_RETRY_BACKOFF_CONFIG_KEY)) config.getLong(MAX_RETRY_BACKOFF_CONFIG_KEY) else DEFAULT_RETRY_BACKOFF_MS
@@ -40,21 +48,24 @@ class Writer private[backend](client: RestHighLevelClient,
         val indexRequest = new IndexRequest(idxName, ES_INDEX_TYPE, al.id)
         indexRequest.source(convertAnomalyToJsonMap(al.anomaly))
         bulkRequest.add(indexRequest)
-        this.client.bulkAsync(bulkRequest, new BulkActionListener(bulkRequest, callback, 0))
+        this.client.bulkAsync(bulkRequest, new BulkActionListener(esWriteTime.time(), bulkRequest, callback, 0))
       }
     } catch {
       case ex: Exception => callback.onComplete(ex)
     }
   }
 
-  private[backend] class BulkActionListener(bulkRequest: BulkRequest,
+  private[backend] class BulkActionListener(timer : Timer.Context ,
+                                            bulkRequest: BulkRequest,
                                             callback: WriteCallback,
                                             retryCount: Int) extends ActionListener[BulkResponse] {
     def onResponse(bulkItemResponses: BulkResponse): Unit = {
       if (bulkItemResponses.hasFailures) {
+        timer.close()
         retry(new RuntimeException("Fail to execute the elastic search write with partial failures:" + bulkItemResponses.buildFailureMessage))
       }
       else {
+        timer.close()
         callback.onComplete(null)
       }
     }
@@ -64,12 +75,13 @@ class Writer private[backend](client: RestHighLevelClient,
     private def retry(e: Exception) = {
       if (retryCount < maxRetries) try {
         Thread.sleep(retryBackOffMillis)
-        client.bulkAsync(bulkRequest, new BulkActionListener(bulkRequest, callback, retryCount + 1))
+        client.bulkAsync(bulkRequest, new BulkActionListener(esWriteTime.time(), bulkRequest, callback, retryCount + 1))
       } catch {
         case e: Exception =>
           callback.onComplete(e)
       }
       else {
+        esWriteFailureMeter.mark()
         logger.error("All retries while writing to elastic search have been exhausted")
         callback.onComplete(e)
       }
